77bab4fa6ecb940c482ece11d251b86aa21aab92
Kolokolova_Vik <2645436@gmail.com>
2024-07-08 17:31:41 +0300

    Commit1
    DZ2.1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConditionalOperatorsHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int repetitionsCount;
+            string message;
+
+            Console.WriteLine("Введите текст:");
+            message = Console.ReadLine();
+            Console.WriteLine("Сколько раз повторить вывод сообщения?");
+            repetitionsCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < repetitionsCount; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
